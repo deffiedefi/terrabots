b1355cf3011b91cf9f8c23dc14c865ac846c1d26
deffiedefi <76966041+deffiedefi@users.noreply.github.com>
2021-10-16 12:19:59 -0700

    info added
    added summary of project, edited css spacing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,6 +12,9 @@ const App = () => {
                 <a class = "subnav" href="#contact">Docs </a>
                 <a class = "subnav" href="#about">Discord </a>
             </div>
+            <p class = "info">Terrabots is a 3D pfp project that uses the cloud to render unique mechanical
+            faces based on a set of randomized parameters. Only 5000 will be minted. Create a terrabot 
+            of your own, mint it, and claim your stake in the future of the generative 3D art world.</p>
             <a href="something" class="button3">Generate</a>
             <a href="https://www.instagram.com/deffiedeff/" class="signature">©DEFFIE 2021</a>
         </div>
